11ede410206095edb8dc549b223e7f2d8308394c
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2024-07-24 17:40:43 -0300

    24072024
    Criação da pagina de gestão dos agendamentos.

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,18 +1,3 @@
-/*import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCjikfZGyH08hxyNq9lFbeW_nnZKToMDfs",
-    authDomain: "barbearia-632bf.firebaseapp.com",
-    projectId: "barbearia-632bf",
-    storageBucket: "barbearia-632bf.appspot.com",
-    messagingSenderId: "900539097858",
-    appId: "1:900539097858:web:2b92d32cdb3c209fa5581b",
-    measurementId: "G-GK6S7FYXYS"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);*/
 
 const btnAgendamento = document.querySelector(".btnAgendamento");
 btnAgendamento.addEventListener("click", async function() {
@@ -36,4 +21,9 @@ btnAgendamento.addEventListener("click", async function() {
     } else {
         alert("Por favor, preencha todos os campos.");
     }
+});
+
+const btnMeuAcesso = document.querySelector("#btnMeuAcesso");
+btnMeuAcesso.addEventListener("click", function() {
+    window.location = './html/gestao.html'
 });
---
+++ js/gestao.js
@@ -0,0 +1,5 @@
+
+const btnVoltar = document.querySelector("#btnVoltar");
+btnVoltar.addEventListener('click', function() {
+    window.location = '../index.html'
+});
