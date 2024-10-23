7e81958175ee48aa8581e1f80aaee6dab4d57600
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2024-10-22 22:54:58 -0300

    Revert "22102024"
    This reverts commit a44d3db91c15821890fc591e43781e15819d0cf9.

--- js/gestao.js
+++ js/gestao.js
@@ -4,20 +4,20 @@ import { getFirestore, collection, query, where, getDocs } from "https://www.gst
 let nomeUsuarioLogado; // Variável global para armazenar o nome do usuário logado
 
 async function verificarLogin(usuario, password) {
+    // Configuração do Firebase
     const firebaseConfig = {
-        apiKey: import.meta.env.VITE_API_KEY,
-        authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_APP_ID,
-        measurementId: import.meta.env.VITE_MEASUREMENT_ID
+        apiKey: "AIzaSyCjikfZGyH08hxyNq9lFbeW_nnZKToMDfs",
+        authDomain: "barbearia-632bf.firebaseapp.com",
+        projectId: "barbearia-632bf",
+        storageBucket: "barbearia-632bf.appspot.com",
+        messagingSenderId: "900539097858",
+        appId: "1:900539097858:web:2b92d32cdb3c209fa5581b",
+        measurementId: "G-GK6S7FYXYS"
     };
-    
-    // Inicializar Firebase
-    const app = initializeApp(firebaseConfig); 
+
+    // Inicialização do Firebase
+    const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
-    
     
     // Consulta no Firestore para verificar se o usuário e senha estão corretos
     const usersRef = collection(db, "baberDigital");
